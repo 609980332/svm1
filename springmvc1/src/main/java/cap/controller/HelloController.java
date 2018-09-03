09edf9d057fbbf45b03e709b21c5370e7977a28b
yf <609980332@qq.com>
2018-09-03 16:58:46 +0800

    test

--- springmvc1/src/main/java/cap/controller/HelloController.java
+++ springmvc1/src/main/java/cap/controller/HelloController.java
@@ -1,8 +1,8 @@
 package cap.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HelloController {
